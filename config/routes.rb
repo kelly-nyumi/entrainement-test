8d1dd55f66726f8e3e3ce442f9464e9d5f49b5c7
Kelly Nerestan <jck.martinique@yahoo.com>
2022-12-08 06:21:16 -0400

    entrainement

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
+  #root to: "pages#home"
+  get "movies", to: "movies#index"
+  get "movies/:id", to: "movies#show", as: :movie
+  get "movies/new", to: "movies#new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
